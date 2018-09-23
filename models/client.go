73feb733f7a50da5e35de7a6c1263a364d308694
Harold <harold2111@gmail.com>
2018-09-22 19:28:26 -0500

    fixing case in json identity and telephone2

--- models/client.go
+++ models/client.go
@@ -7,10 +7,11 @@ type Client struct {
 	CreatedAt      time.Time  `json:"-"`
 	UpdatedAt      time.Time  `json:"-"`
 	DeletedAt      *time.Time `sql:"index" json:"-"`
-	Identification string     `gorm:"not null; unique_index"`
+	Identification string     `gorm:"not null; unique_index" json:"identification" validate:"required"`
 	FirstName      string     `gorm:"not null" json:"firstName" validate:"required"`
 	LastName       string     `gorm:"not null" json:"lastName" validate:"required"`
 	Telephone1     string     `gorm:"not null" json:"telephone1" validate:"required"`
+	Telephone2     string     `json:"telephone2,omitEmpty"`
 	Email          string     `json:"email" validate:"required"`
 	Addresses      []Address  `validate:"dive" json:"addresses,omitempty" `
 }
